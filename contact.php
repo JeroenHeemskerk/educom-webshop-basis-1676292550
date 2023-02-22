72af5067cb3d3f8af5c7ccdb9ef32f7ff51d6999
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-02-22 13:45:23 +0100

    04-B: Opdracht 2.1: Structuur aanbrengen in de code en routering toevoegen #7 finished

--- contact.php
+++ contact.php
@@ -24,41 +24,42 @@ function showContactContent()
         $nameErr = "Only letters and white space allowed";
       }
     }
-  }
 
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
+
+    if (empty($_POST["email"])) {
+      $emailErr = "Email is required";
+    } else {
+      $email = test_input($_POST["email"]);
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+      }
     }
-  }
-  if (empty($_POST["phone"])) {
-    $phoneErr = "Phone is required";
-  } else {
-    $phone = test_input($_POST["phone"]);
-  }
-  if (empty($_POST["salutation"])) {
-    $salutation = "";
-  } else {
-    $salutation = test_input($_POST["salutation"]);
-  }
+    if (empty($_POST["phone"])) {
+      $phoneErr = "Phone is required";
+    } else {
+      $phone = test_input($_POST["phone"]);
+    }
+    if (empty($_POST["salutation"])) {
+      $salutation = "";
+    } else {
+      $salutation = test_input($_POST["salutation"]);
+    }
 
-  if (!empty($_POST["contactOption"]) && isset($contactOption)) {
-    $contactOption = test_input($_POST["contactOption"]);
-  } else {
-    $contactOptionErr = "Contact option is required";
-  }
-  if (empty($_POST["message"])) {
-    $messageErr = "Message is required";
-  } else {
-    $message = test_input($_POST["message"]);
-  }
+    if (!empty($_POST["contactOption"]) && isset($contactOption)) {
+      $contactOption = test_input($_POST["contactOption"]);
+    } else {
+      $contactOptionErr = "Contact option is required";
+    }
+    if (empty($_POST["message"])) {
+      $messageErr = "Message is required";
+    } else {
+      $message = test_input($_POST["message"]);
+    }
 
-  if (strcmp($nameErr, '') == 0 && strcmp($emailErr, '') == 0 && strcmp($phoneErr, '') == 0 && strcmp($contactOptionErr, '') == 0 && strcmp($messageErr, '') == 0) {
-    $valid = true;
-  };
+    if (strcmp($nameErr, '') == 0 && strcmp($emailErr, '') == 0 && strcmp($phoneErr, '') == 0 && strcmp($contactOptionErr, '') == 0 && strcmp($messageErr, '') == 0) {
+      $valid = true;
+    };
+  }
 
   if (!$valid) { /* Show the next part only when $valid is false */
     echo '
@@ -66,59 +67,72 @@ function showContactContent()
           <form method="post" action="index.php">
             <fieldset>
               <legend>Personal</legend>
-              <label for="name"><span>Name <span class="required">*</span></span><input type="text" class="input-field" name="name" value="' . $name . '" /></label>
+              <label for="name"><span class="required">Name*</span><input type="text" class="input-field" name="name" value="' . $name . '" /></label>
               <span class="error">' . $nameErr . '</span>
-              <label for="email"><span>Email <span class="required">*</span></span><input type="email" class="input-field" name="email" value="' . $email . '" /></label>
+              <label for="email"><span class="required">Email*</span><input type="email" class="input-field" name="email" value="' . $email . '" /></label>
               <span class="error">' . $emailErr . '</span>
-              <label for="phone"><span>Phone <span class="required">*</span></span><input type="text" class="input-field" name="phone" value="' . $phone . '" /></label>
+              <label for="phone"><span class="required">Phone*</span><input type="text" class="input-field" name="phone" value="' . $phone . '" /></label>
               <span class="error">' . $phoneErr . '</span>
               <label for="salutation"><span>How can we address you?</span><select name="salutation" class="select-field">
-                  <option '; ?> <?php if ($salutation == "Mrs") {
-                                  echo "selected";
-                                } ?><?php echo 'value="Mrs">Mrs</option>
-                  <option '; ?><?php if ($salutation == "Ms") {
-                                  echo "selected";
-                                } ?><?php echo 'value="Ms">Ms</option>
-                  <option '; ?><?php if ($salutation == "Mx") {
-                                  echo "selected";
-                                } ?><?php echo 'value="Mx">Mx</option>
-                  <option '; ?><?php if ($salutation == "Mr") {
-                                  echo "selected";
-                                } ?><?php echo 'value="Mr">Mr</option>
+                  <option ';
+    if ($salutation == "Mrs") {
+      echo "selected";
+    }
+    echo 'value="Mrs">Mrs</option>
+                  <option ';
+    if ($salutation == "Ms") {
+      echo "selected";
+    }
+    echo 'value="Ms">Ms</option>
+                  <option ';
+    if ($salutation == "Mx") {
+      echo "selected";
+    }
+    echo 'value="Mx">Mx</option>
+                  <option ';
+    if ($salutation == "Mr") {
+      echo "selected";
+    }
+    echo 'value="Mr">Mr</option>
                 </select></label>
             </fieldset>
             <fieldset>
               <legend>Preferred contact option *:</legend>
-              <label for="email"><span>email</span><input' ?><?php if ($contactOption == "email") {
-                                                                echo "checked";
-                                                              } ?> <?php echo 'type="radio" id="email" name="contactOption" class="required" value="email"></label><br>
-              <label for="phone"><span>phone</span><input' ?><?php if ($contactOption == "phone") {
-                                                                echo "checked";
-                                                              } ?> <?php echo 'type="radio" id="phone" name="contactOption" class="required" value="phone"></label><br>
+              <label for="email"><span>email</span></label>
+              <input type="radio" id="email" name="contactOption" class="required"';
+
+    if ($contactOption == "email") {
+      echo "checked";
+    }
+    echo ' value="email"><br>
+              <label for="phone"><span>phone</span></label>
+              <input type="radio" id="phone" name="contactOption" class="required"';
+    if ($contactOption == "phone") {
+      echo "checked";
+    }
+    echo ' value="phone"><br>
               <span class="error">' . $contactOptionErr . '</span>
             </fieldset>
             <fieldset>
               <legend>How can I help you?</legend>
-              <label for="message"><span>Message <span class="required">*</span></span><textarea name="message" class="textarea-field">' . $message . '</textarea></label>
+              <label for="message"><span class="required">Message*</span><textarea name="message" class="textarea-field">' . $message . '</textarea></label>
               <span class="error">' . $messageErr . '</span>
               <p></p>
               <label><input type="submit" value="Submit" /></label>
-            </fieldset>
+              <input type="hidden" name="page" value="contact">                                                
+              </fieldset>
           </form>
         </div>'
-                                                                    ?> <?php } else { /* Show the next part only when $valid is true */
-                                                                        echo '
+?> <?php } else { /* Show the next part only when $valid is true */
+    echo '
         <p>Thank you for your reply!</p>
 
-        <div>Name: ' . $salutation . $name . '</div>
+        <div>Name: ' . $salutation . " " . $name . '</div>
         <div>Email: ' . $email . ' </div>
         <div>Phone: ' . $phone . '</div>
         <div>Preferred Contact Option: ' . $contactOption . '</div>
-        <div>Message: ' . $message . '</div>
-
-
-        /* End of conditional showing */ 
+        <div>Message: ' . $message . '</div>        
     </div> ';
-                                                                      }
-                                                                    }
-                                                                        ?>
+  }
+}
+    ?>
--- index.php
+++ index.php
@@ -53,47 +53,63 @@ function showHeadSection($page)
 {
 
     echo '<head>
-    <title>' . $page . '</title>
-    <link rel="stylesheet" href="style.css">
+    <title>' . strtoupper($page) . '</title>
+    <link rel="stylesheet" href="CSS/style.css">
     <link rel="icon" type="image/x-icon" href="C:\xampp\htdocs\educom-webshop-basis\favicon.ico">
   </head>';
 }
 
+// =================================================================
+// Functions for Body
+// =================================================================
+
 function showBodySection($page)
 {
     echo '    <body>' . PHP_EOL;
+    echo '<div id="page-container">
+    <div id="content-wrap">' . PHP_EOL;
     showHeader($page);
     showMenu();
     showContent($page);
+    echo ' </div>' . PHP_EOL;
     showFooter();
+    echo '</div>' . PHP_EOL;
     echo '    </body>' . PHP_EOL;
-}
-
-function endDocument()
-{
-    echo  '</html>';
 }
 
 function showHeader($page)
 {
-    echo '<header>
-    <h1>' . $page . '</h1>
-  </header>';
+    switch ($page) {
+        case 'home':
+        case 'about':
+        case 'contact':
+            echo '<header>
+        <h1>' . strtoupper($page) . '</h1>
+      </header>';
+            break;
+        default:
+            echo '<h1>Page not found</h1>';
+            break;
+    }
 }
 
 function showMenu()
 {
     echo '<nav>
     <ul class="menu">
-    <a href="index.php?page=home>
-            <li>HOME</li>
+    <a href="index.php?page=home">
+    <li>HOME</li>
+    </a>
+    <a href="index.php?page=about">
+    <li>ABOUT</li>
+    </a>
+    
+    <a href="index.php?page=contact">
+    <li>
+            CONTACT
+            </li> 
         </a>
-        <a href="index.php?page=about>
-            <li>ABOUT</li>
-        </a>
-        <a href="index.php?page=contact>
-            <li>CONTACT</li>
-        </a>
+        
     </ul>
 </nav>';
 }
@@ -112,6 +128,9 @@ function showContent($page)
         case 'contact':
             require('contact.php');
             showContactContent();
+            break;
+        default:
+            echo '<h1>This page does not exist</h1>';
     }
 }
 
@@ -123,4 +142,11 @@ function showFooter()
         document.write(new Date().getFullYear())
       </script> Lydia van Gammeren All Rights Reserved</p>
   </footer>';
+}
+
+// =================================================================
+
+function endDocument()
+{
+    echo  '</html>';
 }
